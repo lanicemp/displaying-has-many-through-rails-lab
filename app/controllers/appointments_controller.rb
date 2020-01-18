80cc4300f566ed99e8915e90e473bb8d5b89ec52
LaNice Powell <lanicemp@aol.com>
2020-01-18 00:31:02 -0500

    Done.

---
+++ app/controllers/appointments_controller.rb
@@ -0,0 +1,30 @@
+class AppointmentsController < ApplicationController
+
+    # def new 
+    #     @appointment = Appointment.new
+    #     @doctor = @appointment.doctor
+    #     @patient = @appointment.patient
+        
+    # end 
+
+    # def create
+    #     Appointment.create(appointment_params)
+    #     redirect_to appointment_path
+    # end 
+ 
+
+    def show 
+        @appointment= Appointment.find(params[:id])
+        @doctor = @appointment.doctor
+        @patient = @appointment.patient
+    end 
+
+    # private 
+
+    # def appointment_params
+    #     params.require(:appointment)
+    # end 
+
+
+
+end
